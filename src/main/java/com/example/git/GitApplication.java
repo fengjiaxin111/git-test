f48f1f5cda3a81899a9c8f99556688d4c13814f3
feng <you@example.com>
2023-06-07 11:03:40 +0800

    hot commit

--- src/main/java/com/example/git/GitApplication.java
+++ src/main/java/com/example/git/GitApplication.java
@@ -8,7 +8,7 @@ public class GitApplication {
 
     public static void main(String[] args) {
         System.out.println("hello,word");
-
+        System.out.println("hot-fix");
         SpringApplication.run(GitApplication.class, args);
     }
 
